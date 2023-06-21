803c1ece3d13ae30dae40a724c9a3f1131f5071e
Yahav Kosoi <yahav.kosoi@gmail.com>
2023-06-21 21:47:11 +0300

    add comments

--- pidControl.cpp
+++ pidControl.cpp
@@ -1,72 +1,87 @@
 #include "pidControl.h"
 
-int rMP1;
-int rMP2;
-int rMPP;
+int rMP1; // rightMotorPin1.
+int rMP2; // rightMotorPin2.
+int rMPP; // rightMotorPowerPin.
 
-int lMP1;
-int lMP2;
-int lMPP;
+int lMP1; // leftMotorPin1.
+int lMP2; // leftMotorPin2.
+int lMPP; // leftMotorPowerPin.
 
+// define variables for PIDcalc().
 double integral = 0;
 double derivative;
 double error;
 double previousError = 0;
-
 double out;
+
+// define variables for goToAngle.
 int tempOut;
 int processValue;
-
 int speedR;
 int speedL;
 
 pidControl::pidControl(int inR1, int inR2, int enR, int inL1, int inL2, int enL){
+  // right motor pins.
   rMP1 = inR1;
   rMP2 = inR2;
   rMPP = enR;
 
+  // left motor pins.
   lMP1 = inL1;
   lMP2 = inL2;
   lMPP = enL;
 
+  // initialize right motor pins.
   pinMode(rMP1, OUTPUT);
   pinMode(rMP2, OUTPUT);
   pinMode(rMPP, OUTPUT);
 
+  // intialize left motor pins.
   pinMode(lMP1, OUTPUT);
   pinMode(lMP2, OUTPUT);
   pinMode(lMPP, OUTPUT);
 }
 
+// spins right motor at (speed).
 void pidControl::rightMotor(int speed){
+  // change motor speed to (speed).
   analogWrite(rMPP, abs(speed));
+  // if speed > 0 turn forward.
   if (speed>0) {
     digitalWrite(rMP1, LOW);
     digitalWrite(rMP2, HIGH);
   }
+  // else turn backward.
   else {
     digitalWrite(rMP1, HIGH);
     digitalWrite(rMP2, LOW);
   }
 }
 
+// spins left motor at (speed).
 void pidControl::leftMotor(int speed) {
+  // change motor speed to (speed).
   analogWrite(lMPP, abs(speed));
+  // if speed > 0 turn forward.
   if (speed>0) {
     digitalWrite(lMP1, HIGH);
     digitalWrite(lMP2, LOW);
   }
+  // else turn backward.
   else {
     digitalWrite(lMP1, LOW);
     digitalWrite(lMP2, HIGH);
   }
 }
 
+// turn both motors at speeds (rightSpeed, leftSpeed).
 void pidControl::move(int rightSpeed, int leftSpeed) {
   rightMotor(rightSpeed);
   leftMotor(leftSpeed);
 }
 
+// stop motors.
 void pidControl::stopMoving(){
   digitalWrite(lMP1, LOW);
   digitalWrite(lMP2, LOW);
@@ -74,51 +89,71 @@ void pidControl::stopMoving(){
   digitalWrite(rMP2, LOW);
 }
 
+// calculate PID output.
 double pidControl::PIDcalc(double PV, int sp, double Kp, double Ki, double Kd){
+  // set error to set_point - process_value.
   error = sp - PV;
+  // add error to integral.
   integral += error;
+  // set derivative to ∆error.
   derivative = error - previousError;
 
+  // multiple each part of PID by Kp, Ki, Kd.
   out = error*Kp + integral*Ki + derivative*Ki;
 
+  // set previous error to current error.
   previousError = error;
 
   return out;
 }
 
+// go to angle using PID.
 void pidControl::goToAngle(int deg, int speed, double Kp, double Ki, double Kd){
+  // set process value to current yaw angle.
   processValue = getYaw();
+
+  // repeat until current yaw angle == deg.
   while (processValue != deg) {
+    // set process value to current yaw angle.
     processValue = getYaw();
 
+    // set tempOut to PIDcalc output.
     tempOut = PIDcalc(processValue, deg, Kp, Ki, Kd);
 
+    // set motor speeds to be opposite of each other.
     speedR = (speed + tempOut) * -1;
     speedL = (speed + tempOut);
 
+    // cant have a speed over 255.
     if (speedR > 255) {
       speedR = 255;
     }
 
+    // cant have a speed less than -255.
     else if (speedR < -255) {
       speedR = -255;
     }
 
+    // cant have a speed over 255.
     if (speedL > 255) {
       speedL = 255;
     }
-
+    
+    // cant have a speed less than -255.
     else if (speedL < -255) {
       speedL = -255;
     }
-
+    // for testing purposes print the speed of each motor.
     Serial.print(speedR);
     Serial.print(" <- speedR, speedL -> ");
     Serial.println(speedL);
+
+    // move both motors at their speed.
     move(speedR, speedL);
   }
 }
-
+// get current yaw angle.
 int pidControl::getYaw(){
+  // for testing return yaw angle of 0˚.
   return 0;
 }
